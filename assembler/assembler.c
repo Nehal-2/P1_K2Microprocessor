a363a358225a40b0925fd2cc7f1ee006ef6b24de
Nehal <NehalAlsuhaibani@gmail.com>
2024-11-10 15:41:24 +0300

    Assembler modification

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-typedef struct {
-    char *instruction;
-    char *machine_code;
-} Instruction;
+#define MAX_IMMEDIATE_VALUE 7
+#define INSTRUCTION_LENGTH 8
 
-Instruction instructions[] = {
-    {"RA=0", "00001000"},
-    {"RB=1", "00011001"},
-    {"RO=RA", "00100000"},
-    {"RB=RA+RB", "00101000"},
-    {"JC=0", "01110000"},
-    {"RA=RA+RB", "00000000"},
-    {"RB=RA-RB", "00010100"},
-    {"RA=RA-RB", "00000100"},
-    {"J=2", "10110010"}
-};
+void remove_spaces(char *str) {
+	char *str2 = str;
+	while (*str) {
+		if (!isspace((unsigned char)*str)) {
+			*str2++ = *str; // Copy non-space characters
+		}
+		*str++;
+	}
+	*str2 = '\0'; // Terminate the space-free string
+}
 
-#define NUM_INSTRUCTIONS (sizeof(instructions) / sizeof(Instruction))
+const char* generate_machine_code(const char *instruction, char *machine_code) {
+	int imm_value = 0;
 
-const char* get_machine_code(const char *instruction) {
-    for (int i = 0; i < NUM_INSTRUCTIONS; i++) {
-        if (strcmp(instruction, instructions[i].instruction) == 0) {
-            return instructions[i].machine_code;
-        }
-    }
-    return NULL;
+	if (strcmp(instruction, "RA=RA+RB") == 0) {
+		strncpy(machine_code, "00000000", INSTRUCTION_LENGTH + 1);
+	} else if (strcmp(instruction, "RB=RA+RB") == 0) {
+		strncpy(machine_code, "00010000", INSTRUCTION_LENGTH + 1);
+	} else if (strcmp(instruction, "RA=RA-RB") == 0) {
+		strncpy(machine_code, "00000100", INSTRUCTION_LENGTH + 1);
+	} else if (strcmp(instruction, "RB=RA-RB") == 0) {
+		strncpy(machine_code, "00010100", INSTRUCTION_LENGTH + 1);
+	} else if (strcmp(instruction, "RO=RA") == 0) {
+		strncpy(machine_code, "00100000", INSTRUCTION_LENGTH + 1);
+	} else if (sscanf(instruction, "RA=%d", &imm_value) == 1 && imm_value <= MAX_IMMEDIATE_VALUE) {
+		strncpy(machine_code, "00001III", INSTRUCTION_LENGTH + 1);
+	} else if (sscanf(instruction, "RB=%d", &imm_value) == 1 && imm_value <= MAX_IMMEDIATE_VALUE) {
+		strncpy(machine_code, "00011III", INSTRUCTION_LENGTH + 1);
+	} else if (sscanf(instruction, "JC=%d", &imm_value) == 1 && imm_value <= MAX_IMMEDIATE_VALUE) {
+		strncpy(machine_code, "01111III", INSTRUCTION_LENGTH + 1);
+	} else if (sscanf(instruction, "J=%d", &imm_value) == 1 && imm_value <= MAX_IMMEDIATE_VALUE) {
+		strncpy(machine_code, "10111III", INSTRUCTION_LENGTH + 1);
+	} else {
+		return NULL; // Unrecognized input
+	}
+	
+	if (strchr(machine_code, 'I')) {
+		for (int i = 2; i >= 0; --i) {
+			machine_code[strlen(machine_code) - 1 - i] = (imm_value & (1 << i)) ? '1' : '0';
+		}
+	}
+
+	return machine_code;
 }
 
 void assemble(const char *input_filename, const char *output_filename) {
@@ -47,13 +68,15 @@ void assemble(const char *input_filename, const char *output_filename) {
     char line[256];
     int line_number = 0;
     int error_found = 0;
-    while (fgets(line, sizeof(line), input_file)) {
-        line[strcspn(line, "\n")] = 0;
-        const char *machine_code = get_machine_code(line);
+    char machine_code[INSTRUCTION_LENGTH + 1];
 
-        if (machine_code) {
-            fprintf(output_file, "%s\n", machine_code);
-            printf("Line %d: %s -> Machine Code: %s\n", line_number, line, machine_code);
+    while (fgets(line, sizeof(line), input_file)) {
+        remove_spaces(line);
+        const char *generated_code = generate_machine_code(line, machine_code);
+
+	if (generated_code) {
+            fprintf(output_file, "%s\n", generated_code);
+            printf("Line %d: %s -> Machine Code: %s\n", line_number, line, generated_code);
         } else {
             fprintf(stderr, "Error on line %d: Unknown instruction or register %s\n", line_number, line);
             error_found = 1;
